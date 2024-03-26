82566fdddbd3f72786157eb355115a37fd4c5535
vitorhj <90215531+vitorhj@users.noreply.github.com>
2024-03-26 14:34:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -131,8 +131,8 @@ try:
         df_usoselect_filtrado = df_risco_uso.iloc[:, filtro_colunas]
         st.dataframe(df_usoselect_filtrado, hide_index=True)
 
-        #df_usoselect_filtrado = df_usoselect_filtrado.loc[df['ZONA'].isin(ib_zon)
-        #st.dataframe(df_usoselect_filtrado, hide_index=True)
+        df_usoselect_filtrado = df_usoselect_filtrado.loc[df['ZONA'].isin(in_zon)]
+        st.dataframe(df_usoselect_filtrado, hide_index=True)
 
         numero_colunas=df_usoselect_filtrado.shape[1]
 

62f6ca28bc96a35db168950f87e6737d556ddc5c
vitorhj <90215531+vitorhj@users.noreply.github.com>
2024-03-28 18:29:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ try:
         if ib_area > 200 and ib_area <= 500:
             id_uso='200a500'
         if ib_area > 500 and ib_area <= 750:
-            id_uso='500a7500'
+            id_uso='500a750'
         if ib_area > 750 and ib_area <= 1000:
             id_uso='750a1000'        
         if ib_area > 1000:
